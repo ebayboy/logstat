9c4cb8947029b0b0fb73c979395be9b2d73031ea
fanpengfei <fanpengfei@jd.com>
2020-06-19 11:35:17 +0800

    fw

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,22 @@
 using json = nlohmann::json;
 using namespace std;
 
+
+void testJSON()
+{
+    auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+
+    std::string s = j3.dump();
+
+    cout << "s:" << s << endl;
+}
+
 int main(int argc, char const *argv[]) {
     /* code */
     cout << "riskstat start..." << endl;
 
 
-    auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
-
+    testJSON();
 
     cout << "riskstat exit!" << endl;
 
