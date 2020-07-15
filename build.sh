fa7c80536dc210bed58bd352f2cb69b299cc10e4
unknown <fanpengfei@360buyad.local>
2020-07-15 07:19:32 +0100

    fw

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#build hiredis
+function build_hiredis()
+{
+	ls src/libs/*.tar.bz2 | xargs -i tar xf {} -C src/libs/
+	make -C src/libs/hiredis
+	ln -s src/libs/hiredis/hiredis.a src/libs/
+}
+
+
+function build_libevent()
+{
+	BUILD_DIR=/tmp/libevent_build
+	cd src/libs/libevent 
+	mkdir -p $BUILD_DIR
+	./configure --prefix=$BUILD_DIR && make -j4 && make install || exit 1
+	cd - 
+	cp $BUILD_DIR/lib/libevent.a src/libs
+	cp $BUILD_DIR/include/* src/include/
+}
+
+
+build_hiredis
+
+build_libevent
+
+make -j4
+
