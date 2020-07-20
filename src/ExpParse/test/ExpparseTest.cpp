75c3ef65f81ca5cccbd2260ceda7f5c694b17ffa
fanpengfei1 <fanpengfei1@jd.com>
2020-07-20 18:39:14 +0800

    fw
    Signed-off-by: fanpengfei1 <fanpengfei1@jd.com>

--- src/ExpParse/test/ExpparseTest.cpp
+++ src/ExpParse/test/ExpparseTest.cpp
@@ -11,13 +11,11 @@ using namespace std;
 
 int main(int args, char **argv)
 {
-    //ExpParse(string exp, vector<string> seps);
     ExpParse e("stat", "AVG(ip, total, 5)");
     cout << "stat:" << e << endl;
 
     ExpParse e1("risk", "GT(ip_total_5m/all_total_5m,0.1)");
-
-    cout << "e1:" << e1 << endl;
+    cout << "risk:" << e1 << endl;
 
     return 0;
 }
