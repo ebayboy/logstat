a4f839c3c3e0b543b0f7af9f5acf881c071651ab
fanpengfei1 <fanpengfei1@jd.com>
2020-07-23 09:16:37 +0800

    fw
    Signed-off-by: fanpengfei1 <fanpengfei1@jd.com>

--- src/include/util.hpp
+++ src/include/util.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <unistd.h>
 
 inline void readargs(int argc, char *const argv[], std::string &c, std::string &d)
@@ -47,9 +48,9 @@ inline void stringSplit(std::string const s, std::string sep, std::vector<std::s
     strs.push_back(s.substr(pos, s.size() - pos));
 }
 
-inline std::vector<string> split(std::string strTime)
+inline std::vector<std::string> split(std::string strTime)
 {
-    std::vector<string> result;
+    std::vector<std::string> result;
     std::string temp("");
     strTime += ')'; //字符串结束标记，方便将最后一个单词入vector
     for (size_t i = 0; i < strTime.size(); i++)
@@ -65,6 +66,23 @@ inline std::vector<string> split(std::string strTime)
         }
     }
     return result;
+}
+
+std::string GetLocalTime(long timeStamp)  //time(NULL)
+{
+    time_t t = timeStamp;
+    tm *_tm = gmtime(&t);
+    int year = _tm->tm_year + 1900;
+    int month = _tm->tm_mon + 1;
+    int date = _tm->tm_mday;
+    int hh = _tm->tm_hour + 8;
+    int mm = _tm->tm_min;
+    int ss = _tm->tm_sec;
+    char tmp[64];
+
+    sprintf(tmp, "%04d-%02d-%02d %02d:%02d:%02d", year, month, date, hh, mm, ss);
+
+    return std::string(tmp);
 }
 
 #endif // !__UTIL_HPP__
