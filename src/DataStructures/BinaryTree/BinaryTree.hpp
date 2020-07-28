a226e8ce6e073a9eef6791c76cb3329d5ae7217f
fanpengfei1 <fanpengfei1@jd.com>
2020-07-28 10:28:09 +0800

    fw
    Signed-off-by: fanpengfei1 <fanpengfei1@jd.com>

--- src/DataStructures/BinaryTree/BinaryTree.hpp
+++ src/DataStructures/BinaryTree/BinaryTree.hpp
@@ -1,4 +1,28 @@
 #ifndef __BINARY_TREE_H__
 #define __BINARY_TREE_H__
 
+#include <iostream>
+
+struct Node {
+    Node *left;
+    Node *right;
+    int value;
+};
+
+class Tree {
+    void Find(int key);
+    void RemoveNode(int key);
+    int  Max();
+    int Min();
+    int PreNode();
+    int PostNode();
+    int Floor();  //向下取整
+    int Ceilling(); //向上取整
+    void RemoveAll();
+
+    void PreOrder();
+    void InOrder();
+    void PostOrder();
+};
+
 #endif // !__BINARY_TREE_H__
