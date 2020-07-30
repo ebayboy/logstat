22600a13136196826ea085f8fffb60a3b58db4e5
fanpengfei1 <fanpengfei1@jd.com>
2020-07-30 16:51:16 +0800

    fw
    Signed-off-by: fanpengfei1 <fanpengfei1@jd.com>

--- src/DataStructures/RBTree/test/RBTreeTest.cpp
+++ src/DataStructures/RBTree/test/RBTreeTest.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <vector>
 
+#include "RBTree.hpp"
+
 using namespace std;
 
 int main(int args, char **argv)
 {
-    
     cout << "hello world" << endl;
 
     return 0;
